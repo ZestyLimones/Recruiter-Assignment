7b152a43fff8063a1963ce3410f83d8873812e49
Micheal Limones <michlimones@gmail.com>
2020-03-28 17:06:47 -0600

    console log results

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 'use strict';
 
-const searchURL = "https://api.github.com/users/"
+const searchURL = "https://api.github.com/users"
 
-function getRepos() {
+function formatPath(path) {
+    const pathItem = Object.keys(path)
+    .map(key => `${path[key]}`)
+    return pathItem;
+}
+
+function getRepos(userHandle,) {
+    const path = {
+        userHandle,
+    }
+
+    const pathString = formatPath(path);
+    const url = searchURL + "/" + pathString + "/repos";
+
+    console.log(url);
 
     fetch(url)
-    .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
+    .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error(response.statusText);
+      })
+      .then(responseJson => console.log(responseJson))
+      .catch(err => {
+        $('#js-error-message').text(`Something went wrong: ${err.message}`);
+      });
 }
-$(getRepos);
+
+function watchForm(){
+    $('form').submit(event => {
+        event.preventDefault();
+        const searchUser = $('#js-user-search').val();
+        getRepos(searchUser,);
+    })
+}
+
+$(watchForm);
